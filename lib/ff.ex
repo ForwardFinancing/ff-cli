1b817252e79912551cc896458a7d5b99c87e2db0
Nathan Lilienthal <nathan@nixpulvis.com>
2018-12-12 17:20:37 -0500

    Small refactor.

--- lib/ff.ex
+++ lib/ff.ex
@@ -33,6 +33,31 @@ defmodule FF do
 
   def repo_apps, do: @repo_apps
 
+  defmodule Heroku do
+    def releases(app, creds) do
+      {:ok, response} = HTTPoison.get(
+        "https://api.heroku.com/apps/#{app}/releases",
+        [
+          {"Accept", "application/vnd.heroku+json; version=3"},
+          {"Range", "id; order=desc"},
+          {"Authorization", "Bearer #{creds["password"]}"}
+        ])
+      {:ok, releases} = Poison.decode(response.body)
+      releases
+    end
+  end
+
+  defmodule GitHub do
+    def branches(repo, creds) do
+      {:ok, response} = HTTPoison.get(
+        "https://api.github.com/repos/ForwardFinancing/#{repo}/branches",
+        [],
+        hackney: [basic_auth: {creds["login"], creds["password"]}])
+      {:ok, branches} = Poison.decode(response.body)
+      branches
+    end
+  end
+
   defmodule CLI do
     def main(args) do
       { opts, args, _ } = OptionParser.parse(args)
@@ -44,39 +69,39 @@ defmodule FF do
     end
 
     def who_has([app]) do
-      heroku_token = Netrc.read["api.heroku.com"]["password"]
+      heroku_creds = Netrc.read["api.heroku.com"]
       github_creds = Netrc.read["github.com"]
 
-      {:ok, response} = HTTPoison.get(
-        "https://api.heroku.com/apps/#{app}/releases",
-        [
-          {"Accept", "application/vnd.heroku+json; version=3"},
-          {"Range", "id; order=desc"},
-          {"Authorization", "Bearer #{heroku_token}"}
-        ])
-      {:ok, releases} = Poison.decode(response.body)
+      # Get a list of releases of the app.
+      releases = FF.Heroku.releases(app, heroku_creds)
       last_deploy = Enum.find(releases,
         fn(r) ->
           r["description"] |> String.starts_with?("Deploy")
         end)
+
+      # Parse out the last deploy SHA.
       ["Deploy", deployed_sha] = last_deploy["description"] |> String.split(" ")
+
+      # Pull out some other information to display.
       last_deployer = last_deploy["user"]["email"]
       last_deploy_at = Timex.parse(last_deploy["updated_at"], "{ISO:Extended}")
                        |> elem(1)
                        |> Timex.Timezone.convert(Timex.Timezone.local)
 
+      # If there's a map from the given app name to a GitHub repo and we have
+      # GitHub credentials then we can _try_ to find the corresponding branch
+      # name.
       repo_app_map = Enum.find(FF.repo_apps, fn {key, value} -> value == app end)
       if repo_app_map && github_creds do
         repo = repo_app_map |> elem(0)
-        {:ok, response} = HTTPoison.get(
-          "https://api.github.com/repos/ForwardFinancing/#{repo}/branches",
-          [],
-          hackney: [basic_auth: {github_creds["login"], github_creds["password"]}])
-        {:ok, branches} = Poison.decode(response.body)
+
+        # Get all the branches for the repo corresponding to the app.
+        branches = FF.GitHub.branches(repo, github_creds)
+
+        # Find the branch's HEAD commit that matches the deployed commit SHA.
         deployed_branch = Enum.find(branches, fn(b) ->
           b["commit"]["sha"] |> String.starts_with?(deployed_sha) end)["name"]
 
-        # TODO: Make the strings have some color.
         header = "#{repo} ** #{app}"
         IO.puts("#{header}
 #{"-" |> String.duplicate(header |> String.length)}
@@ -84,7 +109,6 @@ Deployed Branch: #{deployed_branch || "Not in sync with GitHub"}
 Deployed By:     #{last_deployer}
 Deployed At:     #{last_deploy_at}")
       else
-        # TODO: Make the strings have some color.
         IO.puts("#{app}
 #{"-" |> String.duplicate(app |> String.length)}
 Deployed Commit: #{deployed_sha}
