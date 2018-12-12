4143b467756230870435ae030b8d3b21087d3079
Nathan Lilienthal <nathan@nixpulvis.com>
2018-12-12 17:09:31 -0500

    Allow apps without a repo in the map.

--- lib/ff.ex
+++ lib/ff.ex
@@ -44,9 +44,9 @@ defmodule FF do
     end
 
     def who_has([app]) do
-      repo = Enum.find(FF.repo_apps, fn {key, value} -> value == app end) |> elem(0)
       heroku_token = Netrc.read["api.heroku.com"]["password"]
       github_creds = Netrc.read["github.com"]
+
       {:ok, response} = HTTPoison.get(
         "https://api.heroku.com/apps/#{app}/releases",
         [
@@ -60,25 +60,37 @@ defmodule FF do
           r["description"] |> String.starts_with?("Deploy")
         end)
       ["Deploy", deployed_sha] = last_deploy["description"] |> String.split(" ")
-
-      {:ok, response} = HTTPoison.get(
-        "https://api.github.com/repos/ForwardFinancing/#{repo}/branches",
-        [],
-        hackney: [basic_auth: {github_creds["login"], github_creds["password"]}])
-      {:ok, branches} = Poison.decode(response.body)
-      deployed_branch = Enum.find(branches, fn(b) ->
-        b["commit"]["sha"] |> String.starts_with?(deployed_sha) end)["name"]
       last_deployer = last_deploy["user"]["email"]
       last_deploy_at = Timex.parse(last_deploy["updated_at"], "{ISO:Extended}")
                        |> elem(1)
                        |> Timex.Timezone.convert(Timex.Timezone.local)
-      # TODO: Make the strings have some color.
-      header = "#{repo} ** #{app}"
-      IO.puts("#{header}
+
+      repo_app_map = Enum.find(FF.repo_apps, fn {key, value} -> value == app end)
+      if repo_app_map && github_creds do
+        repo = repo_app_map |> elem(0)
+        {:ok, response} = HTTPoison.get(
+          "https://api.github.com/repos/ForwardFinancing/#{repo}/branches",
+          [],
+          hackney: [basic_auth: {github_creds["login"], github_creds["password"]}])
+        {:ok, branches} = Poison.decode(response.body)
+        deployed_branch = Enum.find(branches, fn(b) ->
+          b["commit"]["sha"] |> String.starts_with?(deployed_sha) end)["name"]
+
+        # TODO: Make the strings have some color.
+        header = "#{repo} ** #{app}"
+        IO.puts("#{header}
 #{"-" |> String.duplicate(header |> String.length)}
 Deployed Branch: #{deployed_branch || "Not in sync with GitHub"}
 Deployed By:     #{last_deployer}
 Deployed At:     #{last_deploy_at}")
+      else
+        # TODO: Make the strings have some color.
+        IO.puts("#{app}
+#{"-" |> String.duplicate(app |> String.length)}
+Deployed Commit: #{deployed_sha}
+Deployed By:     #{last_deployer}
+Deployed At:     #{last_deploy_at}")
+      end
     end
   end
 end
